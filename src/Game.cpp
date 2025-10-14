a9d060fde7b9d2588d3ec7ca036b9ed402e55168
couvq <quentincouvelaire@gmail.com>
2025-10-14 18:25:55 -0400

    initial game skeleton

---
+++ src/Game.cpp
@@ -0,0 +1,42 @@
+#include "Game.h"
+#include <iostream>
+
+Game::Game()
+{
+    std::cout << "Game constructor called!\n";
+}
+
+Game::~Game()
+{
+     std::cout << "Game destructor called!\n";
+}
+
+void Game::Initialize()
+{
+    // TODO
+}
+
+void Game::Run()
+{
+    // TODO
+}
+
+void Game::ProcessInput()
+{
+    // TODO
+}
+
+void Game::Update()
+{
+    // TODO
+}
+
+void Game::Render()
+{
+    // TODO
+}
+
+void Game::Destroy()
+{
+    // TODO
+}
