2f76b50b278c6a212772120bdf0be1f179a6f977
couvq <quentincouvelaire@gmail.com>
2025-10-31 18:50:35 -0400

    implement system member functions

--- src/ECS/ECS.cpp
+++ src/ECS/ECS.cpp
@@ -1,5 +1,32 @@
 #include "ECS.h"
 
-int Entity::GetId() const {
+int Entity::GetId() const
+{
     return id;
+}
+
+void System::AddEntityToSystem(Entity entity)
+{
+    entities.push_back(entity);
+}
+
+void System::RemoveEntityFromSystem(Entity entity)
+{
+    for (int i = 0; i < entities.size(); i++)
+    {
+        if (entities[i].GetId() == entity.GetId())
+        {
+            entities.erase(entities.begin() + i);
+        }
+    }
+}
+
+std::vector<Entity> System::GetSystemEntities() const
+{
+    return entities;
+}
+
+const Signature &System::GetComponentSignature() const
+{
+    return componentSignature;
 }
